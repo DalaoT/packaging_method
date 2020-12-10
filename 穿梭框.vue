dd58bee887de7faa116d23aae8ee75e3c939b70b
Tanda <54846691+DalaoT@users.noreply.github.com>
2020-12-10 10:04:00 +0800

    Update 穿梭框.vue
    仅供参考,需要element ui依赖

--- 穿梭框.vue
+++ 穿梭框.vue
@@ -1,16 +1,56 @@
 <template>
-          <div class="ta-transfer">
+                <div class="PackView_transfer"
+           v-for="(item,index_1) in bgData"
+           :key="index_1">
+        <el-divider>包裹{{index_1+1}}</el-divider>
+
+        <div class="PackView_transfer_top" v-if="false">
+          <el-form :model="bgData[index_1].zxdInfo"
+                   ref="numberValidateForm"
+                   label-width="100px">
+            <el-form-item label="长度(cm)"
+                          prop="long">
+              <el-input type="number"
+                        v-model.number="bgData[index_1].zxdInfo.long"
+                        autocomplete="off"></el-input>
+            </el-form-item>
+
+            <el-form-item label="宽度(cm)"
+                          prop="long">
+              <el-input type="number"
+                        v-model.number="bgData[index_1].zxdInfo.long"
+                        autocomplete="off"></el-input>
+            </el-form-item>
+
+            <el-form-item label="高度(cm)"
+                          prop="long">
+              <el-input type="number"
+                        v-model.number="bgData[index_1].zxdInfo.long"
+                        autocomplete="off"></el-input>
+            </el-form-item>
+
+            <el-form-item label="重量(kg)"
+                          prop="long">
+              <el-input type="number"
+                        v-model.number="bgData[index_1].zxdInfo.long"
+                        autocomplete="off"></el-input>
+            </el-form-item>
+          </el-form>
+        </div>
+
+        <div class="ta-transfer">
           <div class="ta-transfer_left">
             <div class="transfer-view-head">
               <div class="head_left">
                 <el-checkbox :indeterminate="bgData[index_1].invoking.isIndeterminate"
+                             :disabled='allData.length==0'
                              v-model="bgData[index_1].invoking.checkAll"
                              @change="handleCheckAllChange($event,index_1,true)">待打包物品</el-checkbox>
               </div>
               <div class="head_right">
                 <span>{{bgData[index_1].invoking.selected_num}}</span>
                 <span>/</span>
-                <span>{{dataAll.length}}</span>
+                <span>{{allData.length}}</span>
               </div>
 
             </div>
@@ -42,11 +82,12 @@
           <div class="ta-transfer_right">
             <div class="transfer-view-head">
               <div class="head_left">
-                <!-- :disabled='bgData[0].wpinfo.length==0' -->
+
                 <el-checkbox :indeterminate="bgData[index_1].parameter.isIndeterminate"
+                             :disabled='bgData[index_1].wpinfo.length==0'
                              v-model="bgData[index_1].parameter.checkAll"
                              @change="handleCheckAllChange($event,index_1)">
-                  包裹1
+                  包裹{{index_1+1}}
                 </el-checkbox>
               </div>
               <div class="head_right">
@@ -69,12 +110,95 @@
             </div>
           </div>
         </div>
+
+      </div>
 </template>
 
 <script>
 export default {
+data(){
+return{
+      dataAll: [],
+      wptype: [],
+      allData_copy: '',
+      allData: [
+        {
+          id: 1,
+          name: '手机',
+          wptype: { id: 1, name: '带电', info: '说明' },
+          num: '物品数量',
+          unitprice: '单价',
+          totalamoun: '总价',
+          bakinfo: '备注说明',
+          enname: '英文名',
+          unit: '12',
+          currencty: '货币类型',
+          extendinfo: '扩展类型'
+        },
+        {
+          id: 2,
+          name: '游戏机',
+          wptype: { id: 1, name: '带电', info: '说明' },
+          num: '物品数量',
+          unitprice: '单价',
+          totalamoun: '总价',
+          bakinfo: '备注说明',
+          enname: '英文名',
+          unit: '12',
+          currencty: '货币类型',
+          extendinfo: '扩展类型'
+        },
+        {
+          id: 3,
+          name: '电视机',
+          wptype: { id: 1, name: '带电', info: '说明' },
+          num: '物品数量',
+          unitprice: '单价',
+          totalamoun: '总价',
+          bakinfo: '备注说明',
+          enname: '英文名',
+          unit: '12',
+          currencty: '货币类型',
+          extendinfo: '扩展类型'
+        },
+        {
+          id: 4,
+          name: '空调',
+          wptype: { id: 1, name: '带电', info: '说明' },
+          num: '物品数量',
+          unitprice: '单价',
+          totalamoun: '总价',
+          bakinfo: '备注说明',
+          enname: '英文名',
+          unit: '12',
+          currencty: '货币类型',
+          extendinfo: '扩展类型'
+        }
+      ],
+      bgData: [
+        {
+          invoking: {
+            checkedCities: [],
+            selected_num: 0,
+            isIndeterminate: false,
+            checkAll: false
+          },
+          parameter: {
+            isIndeterminate: false,
+            checkAll: false,
+            checkedCities: [],
+            selected_num: 0,
+            dataAll: []
+          },
+          wpinfo: [
+          ],
+          zxdInfo: {}
+        }
+      ],
+}
+},
  methods:{
-    addRemoveGoods (index) { //添加或者删除物品
+ addRemoveGoods (index) { //添加或者删除物品
       let formData = this.bgData[index].invoking.checkedCities
       for (const key in formData) {
         for (const k in this.allData) {
@@ -84,10 +208,13 @@ export default {
           }
         }
       }
+      for (const key in this.bgData[index].wpinfo) {
+        this.bgData[index].parameter.dataAll.push(this.bgData[index].wpinfo[key].id)
+      }
       this.bgData[index].invoking.selected_num = 0
       this.bgData[index].invoking.isIndeterminate = false
       this.bgData[index].invoking.checkedCities = []
-
+      this.bgData[index].invoking.checkAll = false
     },
     removeGoods (index) { //拿出
       let formData = this.bgData[index].parameter.checkedCities
@@ -101,6 +228,7 @@ export default {
       this.bgData[index].parameter.selected_num = 0
       this.bgData[index].parameter.isIndeterminate = false
       this.bgData[index].parameter.checkedCities = []
+      this.bgData[index].parameter.checkAll = false
 
     },
     // ----------------------------
@@ -110,7 +238,6 @@ export default {
         this.bgData[index].invoking.selected_num = val ? this.dataAll.length : 0
         this.bgData[index].invoking.isIndeterminate = false;
       } else {
-
         this.bgData[index].parameter.checkedCities = val ? this.bgData[index].parameter.dataAll : []
 
         this.bgData[index].parameter.selected_num = val ? this.bgData[index].parameter.dataAll.length : 0
@@ -143,6 +270,20 @@ export default {
 
       }
 
+    },
+    ConfirmSubmit () { //确认提交修改物品信息
+      this.$confirm('确定修改物品信息吗,该修改只用于提交使用,刷新页面会还原数据', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.allData = this.allData_copy
+        this.$message({
+          type: 'success',
+          message: '修改成功!'
+        });
+      })
+    },
     }
 }
 </script >
